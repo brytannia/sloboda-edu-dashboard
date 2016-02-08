c2e9cfa2428dce4dd8b9516bd756697db4a71e7c
Mary Solodovnik <gimiro42@gmail.com>
2016-02-08 16:21:38 +0200

    add active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
     temp = [:first_name, :last_name, :email, :password, :password_confirmation]
     temp.each { |p| devise_parameter_sanitizer.for(:sign_up) << p }
   end
+
+  def authenticate_admin_user!
+    redirect_to new_user_session_path unless current_user.try(:is_admin?)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  def is_admin?
+    self.email && ENV['ADMIN_EMAILS'].to_s.include?(self.email)
+  end
 end
