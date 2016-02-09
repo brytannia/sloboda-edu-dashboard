9197d0c46e32f1e38bfa28f5c1ae43e6ac6a854a
Mary Solodovnik <gimiro42@gmail.com>
2016-02-09 12:57:45 +0200

    add admin panel, edit users, events, locations

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,17 +1,5 @@
-ActiveAdmin.register User do
-  permit_params :first_name, :last_name, :speaker, :email,
-    :password, :password_confirmation
-  form do |f|
-    f.inputs "User" do
-      f.input :first_name
-      f.input :last_name
-      f.input :speaker
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
-    end
-    f.actions
-  end
+ActiveAdmin.register Event do
+	permit_params :datetime, :subject, :confirmed, :location_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
