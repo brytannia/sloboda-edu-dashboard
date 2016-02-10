6cc9dbc43bda86f213e8623e62467c5156acba3c
Mary Solodovnik <gimiro42@gmail.com>
2016-02-10 12:35:45 +0200

    change association and pattern for hidden files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :user_events
+  has_many :events, through: :user_events
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
