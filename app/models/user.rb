849bf3dfd4fef7024b48890d1bf0ae4f37c8e010
Mary Solodovnik <gimiro42@gmail.com>
2016-02-12 13:53:30 +0200

    remove pictures and change tabs to spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,8 +32,4 @@ class User < ActiveRecord::Base
                     styles: { medium: '300x300#', thumb: '150x150#' },
                     default_url: '/images/:style/missing.png'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-
-  def admin?
-    admin
-  end
 end
