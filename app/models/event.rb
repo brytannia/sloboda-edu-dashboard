a0a5207a2e0c3aeeb7f3e1fd7c76fff8a70b3a0b
Mary Solodovnik <gimiro42@gmail.com>
2016-02-18 11:55:55 +0200

    move send_email to the event model and add after create notification

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,14 @@ class Event < ActiveRecord::Base
   accepts_nested_attributes_for :users
   accepts_nested_attributes_for :user_events
   validates_presence_of :subject
+
+  after_create :send_email
+  after_update :send_email
+
+  def send_email
+    event = Event.find(id)
+    # event_time = (event.datetime.to_time - 3.hours).to_datetime
+    UserMailer.delay(run_at: 2.seconds.from_now)
+              .notification_email(User.last, event)
+  end
 end
