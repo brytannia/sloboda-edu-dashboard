0dc0a11b98f5c6716b4513fcbe03c44d8f5f21f4
Mary Solodovnik <gimiro42@gmail.com>
2016-02-19 13:51:04 +0200

    add validation for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -21,6 +21,7 @@ class Event < ActiveRecord::Base
   accepts_nested_attributes_for :users
   accepts_nested_attributes_for :user_events
   validates_presence_of :subject
+  validate :datetime_no_less_than_15_mins_before_now
 
   after_create :send_email
   after_update :send_email
@@ -32,6 +33,12 @@ class Event < ActiveRecord::Base
     users.each do |u|
       UserMailer.delay(run_at: event_time)
                 .notification_email(u, event)
+    end
+  end
+
+  def datetime_no_less_than_15_mins_before_now
+    if datetime <= 15.minutes.ago
+      errors.add(:datetime, "can't be less than 15 minutes from now")
     end
   end
 end
