d68091f542092a8be4a94ed6db3fb0c4dd0d86b1
Mary Solodovnik <gimiro42@gmail.com>
2016-02-25 16:07:47 +0200

    add gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,8 @@
 #
 
 class User < ActiveRecord::Base
+  include Gravtastic
+  gravtastic
   has_many :user_events
   has_many :events, through: :user_events
   devise :database_authenticatable, :registerable,
