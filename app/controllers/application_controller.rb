fa45807fe7fdab007947da162756e8c37abb2125
Mary Solodovnik <gimiro42@gmail.com>
2016-02-26 18:58:54 +0200

    fix for merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_vars
   protect_from_forgery with: :exception
+  before_action :set_vars
 
   protected
 
